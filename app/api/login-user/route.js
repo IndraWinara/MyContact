4b8f370e05eee0ce82adff864c5ebf56b3f5fc60
IndraWinara <winaraindra@gmail.com>
2023-11-02 09:49:32 +0700

    cek-cors

--- app/api/login-user/route.js
+++ app/api/login-user/route.js
@@ -5,41 +5,49 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 
-export const POST = async (request)=>{
+export const POST = async (request) => {
     try {
-    const {email,password} = await request.json()
-    await connectDb()
+        const { email, password } = await request.json()
+        await connectDb()
 
-    const user = await Users.findOne({email})
-    if(!user){
-        return NextResponse.json({
-            success : false,
-            message : 'Invalid Credential'
-        },{status : 400})
-    }
+        const user = await Users.findOne({ email })
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: 'Invalid Credential'
+            }, { status: 400 })
+        }
 
-    const isPasswordMatch = await bcrypt.compare(password,user.password)
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
 
-    if(isPasswordMatch === false){
-        return NextResponse.json({
-            success : false,
-            message : 'Invalid Credential'
-        },{status : 400})
-    }
+        if (isPasswordMatch === false) {
+            return NextResponse.json({
+                success: false,
+                message: 'Invalid Credential'
+            }, { status: 400 })
+        }
 
-    const token = jwt.sign({id:user._id},process.env.TOKEN_SECRET,{expiresIn : '1d'})
-    const response = NextResponse.json({
-        success : true,
-        message : 'Loggin Successfully'
-    },{status : 200})
-    response.cookies.set('token',token,{httpOnly:true})
-  
-    return response
+        const token = jwt.sign({ id: user._id }, process.env.TOKEN_SECRET, { expiresIn: '1d' })
+        const response = NextResponse.json({
+            success: true,
+            message: 'Loggin Successfully'
+        }, {
+            status: 200, headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            }
+        })
+        response.cookies.set('token', token, { httpOnly: true })
+
+        return response
     } catch (error) {
         return NextResponse.json({
-            success : false,
-            message : error.message
-        },{status:500})
+            success: false,
+            message: error.message
+        }, {
+            status: 500,
+        })
     }
-    
+
 }
